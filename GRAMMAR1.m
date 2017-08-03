cf4cc08451496d6d62332b0edb859d2b5cd7aef0
Neils Schoenfelder <neilsschoenfelder@gmail.com>
2017-08-03 01:47:56 -0400

    Clean up tabs and leading spaces

--- GRAMMAR1.m
+++ GRAMMAR1.m
@@ -1,25 +1,25 @@
 GRAMMER1
-        ; Copyright (C) 2013 Neils Schoenfelder
-        ; 
-        ; This program is free software; you can redistribute it and/or
-        ; modify it under the terms of the GNU General Public License
-        ; as published by the Free Software Foundation; either version 2
-        ; of the License, or (at your option) any later version.
-        ; 
-        ; This program is distributed in the hope that it will be useful,
-        ; but WITHOUT ANY WARRANTY; without even the implied warranty of
-        ; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-        ; GNU General Public License for more details.
-        ; 
-        ; You should have received a copy of the GNU General Public License
-        ; along with this program; if not, write to the Free Software
-        ;   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-        ; 
-        ; 
-        d tester() q  ; Put in a hook that will redirect to the tester
-        ;
-        ;
-        ; =====  OVERVIEW  ====
+    ; Copyright (C) 2013 Neils Schoenfelder
+    ; 
+    ; This program is free software; you can redistribute it and/or
+    ; modify it under the terms of the GNU General Public License
+    ; as published by the Free Software Foundation; either version 2
+    ; of the License, or (at your option) any later version.
+    ; 
+    ; This program is distributed in the hope that it will be useful,
+    ; but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ; GNU General Public License for more details.
+    ; 
+    ; You should have received a copy of the GNU General Public License
+    ; along with this program; if not, write to the Free Software
+    ;   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+    ; 
+    ; 
+    d tester() q  ; Put in a hook that will redirect to the tester
+    ;
+    ;
+    ; =====  OVERVIEW  ====
 	; This routine holds the grammer (BNF-equivalent) for the ANSI 95 version of MUMPS.
 	; This grammer is pulled from the MUMPS annotated standard located on the web here:  http://71.174.62.16/Demo/AnnoStd
 	; This grammer can be used with the code in the ^PARSER1 routine to parse MUMPS code.
